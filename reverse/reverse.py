d585f87e4aae746c125beb45dcc4011cbb55ae14
Jaydenzk <joowan88@gmail.com>
2020-05-15 09:40:39 -0700

    update

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -1,42 +1,49 @@
 class Node:
-    def __init__(self, value=None, next_node=None):
-        self.value = value
-        self.next_node = next_node
+  def __init__(self, value=None, next_node=None):
+    self.value = value
+    self.next_node = next_node
 
-    def get_value(self):
-        return self.value
+  def get_value(self):
+    return self.value
 
-    def get_next(self):
-        return self.next_node
+  def get_next(self):
+    return self.next_node
 
-    def set_next(self, new_next):
-        self.next_node = new_next
+  def set_next(self, new_next):
+    self.next_node = new_next
 
 class LinkedList:
-    def __init__(self):
-        self.head = None
+  def __init__(self):
+    self.head = None
 
-    def add_to_head(self, value):
-        node = Node(value)
+  def add_to_head(self, value):
+    node = Node(value)
 
-        if self.head is not None:
-            node.set_next(self.head)
+    if self.head is not None:
+      node.set_next(self.head)
 
-        self.head = node
+    self.head = node
 
-    def contains(self, value):
-        if not self.head:
-            return False
+  def contains(self, value):
+    if not self.head:
+      return False
 
-        current = self.head
+    current = self.head
 
-        while current:
-            if current.get_value() == value:
-                return True
+    while current:
+      if current.get_value() == value:
+        return True
 
-            current = current.get_next()
+      current = current.get_next()
 
-        return False
+    return False
 
-    def reverse_list(self, node, prev):
-        pass
+  def reverse_list(self):
+    current = self.head
+    prev = None
+    while(current is not None):
+      next = current.next_node
+      current.next_node = prev
+      prev = current
+      current = next
+    self.head = prev
