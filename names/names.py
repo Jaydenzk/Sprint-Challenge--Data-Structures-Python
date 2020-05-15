4fda27b2cf07fb895659d5d86ce5302aeb2897ef
Jaydenzk <joowan88@gmail.com>
2020-05-15 09:42:12 -0700

    updated

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -10,13 +11,21 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+bst = BinarySearchTree(names_1[0])
+for name in names_1[1:]:
+    bst.insert(name)
+
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+for name in names_2:
+    if bst.contains(name):
+        duplicates.append(name)
+
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -24,5 +33,21 @@ print (f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
-# What's the best time you can accomplish?  Thare are no restrictions on techniques or data
-# structures, but you may not import any additional libraries that you did not write yourself.
+# What's the best time you can accomplish with no restrictions on techniques or data
+# structures?
+
+start_time = time.time()
+
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+duplicates_set = set(names_1).intersection(set(names_2))
+
+end_time = time.time()
+print (f"{len(duplicates_set)} duplicates_set:\n\n{', '.join(duplicates_set)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
